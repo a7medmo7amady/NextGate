e286c36aa44474fa6b533cc95c5ce95968280878
a7medmo7amady <ahmedmohamady2005@gmail.com>
2026-03-03 22:32:27 +0200

    Flight Controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,8 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const { connectDB } = require("./config/db");
 const authRoutes = require("./routes/auth.routes");
+const flightRoutes = require("./routes/flight.routes");
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,7 +17,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/flights", flightRoutes);
+dotenv.config();
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
